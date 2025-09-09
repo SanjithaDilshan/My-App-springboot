e47d9873c9d4041373d8ea43c22885cf6f5abc05
Sanjitha Dilshan <sanjudilshan7233@gmail.com>
2025-09-09 23:24:11 +0530

    Dependency injection using Spring Boot

---
+++ src/main/java/com/practice/MyApp/Dev.java
@@ -0,0 +1,11 @@
+package com.practice.MyApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.println("working on Awesome Project");
+    }
+
+}
